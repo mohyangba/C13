96e2ad6756b2ae80ff51f92ad01e4417c5179c42
mohyangba <mohyangba@users.noreply.github.com>
2023-11-30 15:32:42 +0900

    02 error

--- main.c
+++ main.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 //형식 선언  
-struct student     {
-       int ID; //학번
-       char name[100]; //이름
-       float score;//학점
+struct point {
+       int x; 
+       int y;
 };
- 
+
+
 int main(int argc, char *argv[])
 {
   //instance 선언
-  struct student s1 = {123, "JUYEOP", 4.3};
+  struct point p1, p2;
+  int xdiff, ydiff;
+  double dist;
   
-   // 값 초기화 및 다른 값 변경
-   s1.ID = 123456;
-   s1.name[0] = 'c';
-   s1.score = 0.7;
-   // 출력
-   printf("ID = %d", s1.ID);
-   printf("NAME = %s\n", s1.name);
-   printf("grade = %f\n", s1.score);
-   
-   strcpy(s1.name, "bongjun");
-   printf("name2 = %s\n");
-   
+  printf("input p1 coordinate ( x y ) : ");
+  scanf("%d %d", &p1.x, &p1.y);
+  
+  printf("input p2 coordinate ( x y ) : ");
+  scanf("%d %d", &p2.x, &p2.y);
+  
+  //calculate the distance of p1 and p2
+  xdiff = p2.x - p2.x;
+  ydiff = p2.y - p2.y;
+  
+  dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+  
+  printf("distance : %f\n", dist);
+  
   system("PAUSE");	
   return 0;
 }
+
